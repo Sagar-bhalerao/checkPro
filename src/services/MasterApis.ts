ddfc12f4d89268628cf28358a3d526a02a5cd3f2
Sagar <test@123>
2024-12-09 17:30:20 +0530

    The Boiler Resources added

---
+++ src/services/MasterApis.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import { BaseURl } from './Auth.Apis';
+import { toast } from 'react-toastify';
+
+export const GetFilledChecklist = async (data: any) => {
+  try {
+    const response = await axios.post(`${BaseURl}/get-transactions`, data);
+    if (response.status === 200) {
+      return response.data;
+    }
+  } catch (error: any) {
+    toast.error(error.response.data.message);
+  }
+};
